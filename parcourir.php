b707c84c6a2ffd92bda4b9f758773616c4924559
Carissan <Jeancome.carissan@edu.ece.fr>
2022-05-24 18:38:42 +0200

    ptin

--- parcourir.php
+++ parcourir.php
@@ -3,57 +3,190 @@
 echo "<meta charset=\"utf-8\">";
 //identifier votre BDD
 $database = "omnes_sante";
-//identifier votre serveur (localhost), utlisateur (root), mot de passe ("")
+
 $db_handle = mysqli_connect('localhost', 'root', '');
 $db_found = mysqli_select_db($db_handle, $database);
 
- //declaration des variables
- $ID = isset($_POST["ID"])? $_POST["ID"] : "";
- $nom = isset($_POST["nom"])? $_POST["nom"] : "";
- $prenom = isset($_POST["prenom"])? $_POST["prenom"] : "";
- $type_medecin = isset($_POST["type_medecin"])? $_POST["type_medecin"] : "";
- $email = isset($_POST["email"])? $_POST["email"] : "";
- $cabinet = isset($_POST["cabinet"])? $_POST["cabinet"] : "";
- $ville=isset($_POST["ville"])? $_POST["ville"] : "";
- $adresse =isset($_POST["adresse"])? $_POST["adresse"] : "";
- $salle =isset($_POST["salle"])? $_POST["salle"] : "";
- $telephone =isset($_POST["telephone"])? $_POST["telephone"] : "";
- $type_examen =isset($_POST["type_examen"])? $_POST["type_examen"] : "";
- $erreur = "";
- 
-if ($db_found) {
- $sql1 = "SELECT * FROM medecin";
- echo "Liste de médecins: ";
- echo "<br> <br> <br>";
- $result1 = mysqli_query($db_handle, $sql1);
- while ($data1 = mysqli_fetch_assoc($result1)) {
-     echo "Nom:" . $data1['nom'] . '<br>';
-     echo "Prenom: " . $data1['prenom'] . '<br>';
-     echo "Type de médecin: " . $data1['type_medecin'] . '<br>';
-     echo "Email: " . $data1['email'] . '<br>';
-     echo "Cabinet: " . $data1['cabinet'] . '<br>';
-     echo "<br> <br> <br>";
- } //end while
- $sql2 = "SELECT * FROM laboratoire";
- $result2 = mysqli_query($db_handle, $sql2);
- echo "Liste de laboratoirs: ";
- echo "<br> <br> <br>";
- while ($data2 = mysqli_fetch_assoc($result2)) {
-     echo "Nom:" . $data2['nom'] . '<br>';
-     echo "Adresse: " . $data2['adresse'] . ", ". $data2['ville'] . '<br>';
-     echo "Email: " . $data2['email'] . '<br>';
-     echo "Salle: " . $data2['salle'] . '<br>';
-     echo "Numero de téléphone: " . $data2['telephone'] . '<br>';
-     echo "<br> <br> <br>";
- } //end while
- $r1=mysqli_query($db_handle,$sql1);
- $r2=mysqli_query($db_handle,$sql2);
-}
-//si le BDD n'existe pas
-else {
-echo "Database not found";
-} //end else
-//fermer la connection
-mysqli_close($db_handle);
- 
+//declaration des variables
+$Id_exam = isset($_POST["id_examen"]) ? $_POST["id_examen"] : "";
+$Type_exam = isset($_POST["type_examen"]) ? $_POST["type_examen"] : "";
+$Id_labo = isset($_POST["fk_laboratoir"]) ? $_POST["fk_laboratoir"] : "";
+$ID = isset($_POST["id_medecin"]) ? $_POST["id_medecin"] : "";
+$nom = isset($_POST["nom"]) ? $_POST["nom"] : "";
+$prenom = isset($_POST["prenom"]) ? $_POST["prenom"] : "";
+$type_medecin = isset($_POST["type_medecin"]) ? $_POST["type_medecin"] : "";
+$email = isset($_POST["email"]) ? $_POST["email"] : "";
+$cabinet = isset($_POST["cabinet"]) ? $_POST["cabinet"] : "";
+$ville = isset($_POST["ville"]) ? $_POST["ville"] : "";
+$adresse = isset($_POST["adresse"]) ? $_POST["adresse"] : "";
+$salle = isset($_POST["salle"]) ? $_POST["salle"] : "";
+$telephone = isset($_POST["telephone"]) ? $_POST["telephone"] : "";
+$type_examen = isset($_POST["type_examen"]) ? $_POST["type_examen"] : "";
+$erreur = "";
 ?>
+
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>
+        Omnes Santé
+    </title>
+
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="" rel="stylesheet" type="text/css" />
+    <link href="css/menu.css " rel="stylesheet" type="text/css" />
+</head>
+
+
+<body>
+    <div id="wrapper">
+        <div id="header">
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#"><img src="../Omnes-Sante/images/logo.png" /></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    </button>
+                    <div class="collapse navbar-collapse justify-content_between" id="navbarSupportedContent">
+                        <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link" aria-current="page" href="menu.html">Accueil</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="parcourir.php">Parcourir</a>
+                            </li>
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="parcourir.html" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Recherche
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item" href="recherche_medecin.html">Recherche médecin</a>
+                                    </li>
+                                    <li><a class="dropdown-item" href="recherche_examen.html">Recherche laboratoire</a>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="parcourir.html" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Compte
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item" href="connexion.html">Connexion</a>
+                                    </li>
+                                    <li><a class="dropdown-item" href="inscription.html">Créer un compte</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <table class="table table-strip">
+            <tr>
+                <th></th>
+                <th>Nom</th>
+                <th>Prenom</th>
+                <th>Spécialité</th>
+                <th>Email</th>
+                <th>Cabinet</th>
+            </tr>
+            <?php
+            if ($db_found) {
+                $sql1 = "SELECT * FROM medecin";
+                echo "Liste de médecins: ";
+                echo "<br> <br> <br>";
+                $result1 = mysqli_query($db_handle, $sql1);
+                while ($data1 = mysqli_fetch_assoc($result1)) {
+                    echo "<tr>";
+                    echo  "<td>" . $data1['id_medecin'] .  "</td>";
+                    echo  "<td>" . $data1['nom'] .  "</td>";
+                    echo  "<td>" . $data1['prenom'] .  "</td>";
+                    echo " <td>" . $data1['type_medecin'] .  "</td>";
+                    echo  "<td>" . $data1['email'] .  "</td>";
+                    echo  "<td>" . $data1['cabinet'] . "</td>";
+                    echo "</tr>";
+                } //end while
+                $sql2 = "SELECT * FROM laboratoire";
+                $result2 = mysqli_query($db_handle, $sql2);
+                $r1 = mysqli_query($db_handle, $sql1);
+                $r2 = mysqli_query($db_handle, $sql2);
+            }
+            //si le BDD n'existe pas
+            else {
+                echo "Database not found";
+            } //end else
+
+            ?>
+        </table>
+        <table class="table table-strip">
+            <tr>
+                <th>Nom</th>
+                <th>Adresse</th>
+                <th>Email</th>
+                <th>Telephone</th>
+            </tr>
+            <?php
+            if ($db_found) {
+                echo "Liste de laboratoirs: ";
+                echo "<br> <br> <br>";
+                while ($data2 = mysqli_fetch_assoc($result2)) {
+                    echo "<tr>";
+                    echo "<td>" . $data2['nom'] . "</td>";
+                    echo "<td>" . $data2['adresse'] . ", " . $data2['ville'] . "</td>";
+                    echo "<td>" . $data2['email'] . "</td>";
+                    echo "<td>" . $data2['telephone'] . "</td>";
+                    echo "</tr>";
+                } //end while
+            }
+            //si le BDD n'existe pas
+            else {
+                echo "Database not found";
+            }
+            ?>
+        </table>
+        <table class="table table-strip">
+            <tr>
+                <th></th>
+                <th>ID Labo</th>
+                <th>Type examen</th>
+            </tr>
+            <?php
+            if ($db_found) {
+                $sql1 = "SELECT * FROM examen";
+                $result1 = mysqli_query($db_handle, $sql1);
+                echo "<br> <br> ";
+                echo "Liste des examens: ";
+                echo "<br> ";
+                while ($data2 = mysqli_fetch_assoc($result2)) {
+                    echo "<tr>";
+                    echo "<td>" . $data2['id_examen'] . "</td>";
+                    echo "<td>" . $data2['email'] . "</td>";
+                    echo "<td>" . $data2['telephone'] . "</td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "Database not found";
+            }
+            mysqli_close($db_handle);
+            ?>
+        </table>
+
+    </div>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.12.0/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.12.0/js/dataTables.bootstrap5.min.js"></script>
+
+</body>
+
+</html>
